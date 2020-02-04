4ddb60f5aefe4a370f4f23e5b9ca5f4dbcfc6223
brandonhdz <git@github.com>
2020-02-04 03:31:06 -0600

    refactoring, created new modules to split work

--- webApp/app.py
+++ webApp/app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from webApp.vGenerator import genGraphs
+import io
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -6,3 +9,15 @@ app.config['DEBUG'] = True
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/graph1")
+def graph1():
+    '''
+    test use case of returning a graph in html see index.html
+    :return:
+    '''
+
+    fig = genGraphs.test()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
--- webApp/vGenerator/genGraphs.py
+++ webApp/vGenerator/genGraphs.py
@@ -1,6 +1,12 @@
 import boto3
-#import plotly
-
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from random import randint
 
 def test():
-    return "Test Render"
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+    return fig
