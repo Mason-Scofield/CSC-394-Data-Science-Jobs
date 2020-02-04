209f9357ed9c154da98461f79a8f9ff2faea827b
Mark Badel <mbadel98@gmail.com>
2020-02-03 18:42:48 -0600

    test

--- webApp/app.py
+++ webApp/app.py
@@ -3,6 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
