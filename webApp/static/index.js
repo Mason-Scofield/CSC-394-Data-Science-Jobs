14137302f9c81cfbbec0ff1df50ee802e95dd691
Christian Kleinvehn <ckleinvehn@gmail.com>
2020-03-09 06:00:30 -0500

    Made some changes to the UI, nothing major.

--- webApp/static/index.js
+++ webApp/static/index.js
@@ -1,57 +1,128 @@
-var skills = {};
+var skills = {}
+lock_state = 'unlocked';
+
+function valid_slider(value) {
+    for (var i = 1; i <= 5; ++i)
+        if (value == i) return value;
+
+    return 1; // default value
+}
 
 window.addEventListener('load', function() {
-  var sliders = document.getElementsByClassName('slider');
-  
-  for (var i = 0; i != sliders.length; ++i) {      
-    slider(sliders[i].value, i + 1);
-  }
+    document.getElementById('lock').addEventListener('click', function(e) {
+        if (lock_state === 'unlocked') { // lock
+            document.getElementById('summary').style.bottom = '0';
+            e.target.className = 'fas fa-lock';
+            lock_state = 'locked';
+            document.getElementById('summary').onmouseenter = (e) => {};
+            document.getElementById('summary').onmouseleave = (e) => {};
+        }
+        else { // unlock
+            document.getElementById('summary').style.bottom = '-125px';
+            e.target.className = 'fas fa-lock-open';
+            lock_state = 'unlocked';
+            document.getElementById('summary').onmouseenter = (e) => e.target.style.bottom = '0';
+            document.getElementById('summary').onmouseleave = (e) => e.target.style.bottom = '-125px';
+        }
+    });
+
+    var value1 = valid_slider(localStorage.getItem('slider1'));
+    var value2 = valid_slider(localStorage.getItem('slider2'));
+    var value3 = valid_slider(localStorage.getItem('slider3'));
+    var value4 = valid_slider(localStorage.getItem('slider4'));
+    var value5 = valid_slider(localStorage.getItem('slider5'));
+
+    skills = {
+        sliders: {
+            'slider1': [value1, value_to_proficiency(value1)],
+            'slider2': [value2, value_to_proficiency(value2)],
+            'slider3': [value3, value_to_proficiency(value3)],
+            'slider4': [value4, value_to_proficiency(value4)],
+        }
+    }
+
+    Object.defineProperty(skills, 'total', {
+        get() {
+            total = 0;
+            for (var key in this.sliders)
+                total += parseInt(this.sliders[key][0]);
+            return total;
+        }
+    });
+
+    Array.from(document.getElementsByClassName('slider')).forEach((elem, i) => {
+        elem.value = skills.sliders['slider' + (i + 1)][0];
+        slider(elem.value, i + 1);
+    });
 });
 
-function slider(value, index) {
-  id    = 'slider' + String(index);
-  skill = 'skill'  + String(index);
-  
-  skills[id] = value;
-  w3.displayObject(id, skills); // BADGE
+// positions start at 1
+function slider(value, position) {
+  var sliderId      = 'slider'      + position;
+  var valueId       = 'value'       + position;
+  var proficiencyId = 'proficiency' + position;
 
-  var obj = {}
-  obj[skill] = skill_value_to_level(value);
-  
-  // SKILL LEVEL
-  w3.displayObject(skill, obj);
+  console.log(skills);
 
-  // OVERALL PROFICIENCY
-  calculate_proficiency();
+  // VALUE; i.e., 1-5
+  skills.sliders[sliderId][0] = value;   // store into the JS global
+  localStorage.setItem(sliderId, value); // persists between sessions
+  w3.displayObject(valueId, {[valueId]: value_to_years(value)});
+
+  // PROFICIENCY; e.g., Novice
+  skills.sliders[sliderId][1] = value_to_proficiency(value);
+  w3.displayObject(proficiencyId, {[proficiencyId]: skills.sliders[sliderId][1]});
+
+  // ROLE; e.g., Entry-level
+  w3.displayObject('role', {'role': values_to_role(skills.total)});
 }
 
-function calculate_proficiency() {
-  slider_values = 0;
+// 1 --   0-3 years
+// 2 --   4-7 years
+// 3 --  8-11 years
+// 4 -- 12-15 years
+// 5 --   >15 years
+function value_to_years(value) {
+    var years = '0–3';
 
-  for (skill in skills) {
-    if (!skills.hasOwnProperty(skill)) continue;
-    slider_values += Number(skills[skill]);
-  }
-  
-  w3.displayObject('role', {
-    'role' : total_values_to_role(slider_values)
-  });
+    switch (value) {
+    case '5': years =   '>15'; break;
+    case '4': years = '12–15'; break;
+    case '3': years =  '8-11'; break;
+    case '2': years =   '4-7'; break;
+    case '1':
+    default: break;
+    }
+
+    return years;
 }
 
-function skill_value_to_level(value) {
+//   0-3 years -- [0, 1, 2, 3]
+//   4-7 years -- [4, 5, 6, 7]
+//  8-11 years -- [8, 9, 10, 11]
+// 12-15 years -- [12, 13, 14, 15]
+//   >15 years -- [15...]
+function value_to_proficiency(value) {
   var proficiency = 'Novice';
-  if (value == 10)     proficiency = 'Expert';
-  else if (value >= 7) proficiency = 'Advanced';
-  else if (value >= 4) proficiency = 'Intermediate';
-  
+
+  switch (value) {
+  case '5': proficiency = 'Master';       break;
+  case '4': proficiency = 'Expert';       break;
+  case '3': proficiency = 'Advanced';     break;
+  case '2': proficiency = 'Intermediate'; break;
+  case '1':
+  default: break;
+  }
+
   return proficiency;
 }
 
-function total_values_to_role(value) {
-  var role = 'Entry-level'
+function values_to_role(total) {
+  // max possible is 20
+  var role = 'Entry-level';
   
-  if (value >= 33)      role = 'Senior-level';
-  else if (value >= 23) role = 'Junior-level';
+  if      (total >= 16) role = 'Senior-level';
+  else if (total >= 10) role = 'Junior-level';
   
   return role;
 }
