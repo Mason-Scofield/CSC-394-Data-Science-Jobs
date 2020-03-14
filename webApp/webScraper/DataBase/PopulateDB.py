943e3721e704d468206edda5cdaff5188b51e445
Mason-Scofield <masonscofield1997@gmail.com>
2020-03-13 21:57:52 -0500

    cnt

--- webApp/webScraper/DataBase/PopulateDB.py
+++ webApp/webScraper/DataBase/PopulateDB.py
@@ -227,11 +227,25 @@ def query_test(state, role, tech):
 
 
 def count():
-    t = dynamo_db.Table('GitHubJobs')
-    t2 = dynamo_db.Table('USAJobs')
-    table1 = t.scan()
-    table2 = t2.scan()
-    cnt = int(table1['Count']) + int(table2['Count'])
+    cnt = 0
+    git_table = dynamo_db.Table('GitHubJobs')
+    usa_table = dynamo_db.Table('USAJobs')
+    table1 = git_table.scan()
+    table2 = usa_table.scan()
+    data1 = table1['Items']
+    data2 = table2['Items']
+    print(table1['Count'])
+    print(table2['Count'])
+    while 'LastEvaluatedKey' in table1:
+        table1 = git_table.scan(ExclusiveStartKey=table1['LastEvaluatedKey'])
+        data1.extend(table1['Items'])
+
+    while 'LastEvaluatedKey' in table2:
+        table2 = usa_table.scan(ExclusiveStartKey=table2['LastEvaluatedKey'])
+        data2.extend(table2['Items'])
+    cnt = len(data1) + len(data2)
+
+    return cnt
 
     return cnt
 
